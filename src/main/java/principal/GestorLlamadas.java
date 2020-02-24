0ca382d764de1ba212f3b04e4f051f5ab23acda3
al386105 <al386105@github.es>
2020-02-24 20:28:20 +0100

    Cambio de nombre y varias modificaciones

--- src/main/java/principal/GestorLlamadas.java
+++ src/main/java/principal/GestorLlamadas.java
@@ -5,20 +5,26 @@ import datos.llamadas.Llamada;
 import java.util.HashMap;
 
 public class GestorLlamadas {
-    HashMap<String, Llamada> llamadas; //La clave es el numero de telefono del cliente
+    HashMap<String, Llamada> llamadas; //La clave es el NIF del cliente
 
 
     public GestorLlamadas(){
         this.llamadas = new HashMap<String, Llamada>();
     }
 
-    public void efectuarLlamada(String telefonoCliente, String telefonoDestino, int duracion){
-        //Comprobar que el telefono existe:
-        Llamada nueva = new Llamada(telefonoDestino, duracion);
-        llamadas.put(telefonoCliente, nueva);
+    public void efectuarLlamada(String nifCliente, String telefonoDestino, int duracion){
+        //Comprobar que el nifCliente corresponde con uno existe en Clientes:
+        //AQUI TENGO UNA CRISIS EXISTENCIAL:
+        //Tengo que comprobar que nifCliente existe en GestorClientes.clientes y no Se como hacerlo (no puedo acceder a los metodos de la otra clase)
+
+        //Una vez comprobado que si puedo efectuar la llamada:
+        Llamada nuevaLlamada = new Llamada(telefonoDestino, duracion);
+        this.llamadas.put(nifCliente, nuevaLlamada);
     }
 
-    public String listarLlamadas(){
-        return null;
+    public String listarLlamadas(String nifCliente){
+        String llamadas = "";
+        StringBuilder sb =
+        return llamadas;
     }
 }
