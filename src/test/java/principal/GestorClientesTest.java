109c8d8033bc7d7d7da020946b044c3d05d6b67d
al386105 <al386105@github.es>
2020-02-26 21:36:03 +0100

    junit 21:35

--- src/test/java/principal/GestorClientesTest.java
+++ src/test/java/principal/GestorClientesTest.java
@@ -1,15 +1,23 @@
 package principal;
 
+import datos.clientes.Cliente;
 import datos.clientes.Direccion;
+import datos.clientes.Particular;
 import es.uji.www.GeneradorDatosINE;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 
 public class GestorClientesTest {
+    GestorClientes gestorClientes;
 
+    @BeforeAll
     public void inicializaClientes(){
-        GestorClientes gestorClientes = new GestorClientes();
+        gestorClientes = new GestorClientes();
         for(int i = 0; i < 100 ; i++){
             GeneradorDatosINE generadorDatosINE = new GeneradorDatosINE();
             String provincia = generadorDatosINE.getProvincia();
@@ -21,15 +29,16 @@ public class GestorClientesTest {
 
     }
 
-
     @Test
     public void testDevuelveCliente(){
-
-
-
+        Direccion direccion = new Direccion("12005", "Castellon de la plana", "Castelllon");
+        gestorClientes.anadirParticular("miguel", "pardo navarro","692242216","20925403",direccion,"miguelpardo@gmail.com");
+        Cliente miguel = new Particular("miguel", "pardo navarro","692242216","20925403", direccion,"miguelpardo@gmail.com");
+        assertThat(gestorClientes.devuelveCliente("20925403"), is(miguel));
     }
+
     @AfterAll
     public void borrado(){
-        
+        gestorClientes = null;
     }
 }
