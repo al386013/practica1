8711c6b30587e8bd726f7fa176b1f03cd7ef94f6
al386013 <al386013@uji.es>
2020-03-03 13:07:33 +0100

    metodo generico

---
+++ src/main/java/principal/MetodoGenerico.java
@@ -0,0 +1,20 @@
+package principal;
+
+import datos.llamadas.Llamada;
+import interfaces.tieneFecha;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class MetodoGenerico {
+    public static < T extends tieneFecha> Collection< T > tiposIguales(HashSet< T > conjunto, LocalDate fechaIni, LocalDate fechaFin) {
+        Collection< T > res = new HashSet< >();
+        for (T elem : conjunto) {
+            LocalDate fecha = elem.getFecha();
+            if(fecha.isAfter(fechaIni) && fecha.isBefore(fechaFin))
+                res.add(elem);
+        }
+        return res;
+    }
+}
