5624f19283f46fc4e5bb3717fcd96f91e5e6e433
al386013 <al386013@uji.es>
2020-03-27 19:08:04 +0100

    cambio

--- src/main/java/acciones/SeleccionaOpcionCambiarTarifa.java
+++ src/main/java/acciones/SeleccionaOpcionCambiarTarifa.java
@@ -9,20 +9,16 @@ import principal.NifNoExistenteException;
 public class SeleccionaOpcionCambiarTarifa implements Accion {
     @Override
     public void ejecutaAccion(BaseDeDatos baseDeDatos) throws OpcionIncorrectaException {
-        try {
-            System.out.println("\nCONTRATAR UNA TARIFA ESPECIAL");
-            System.out.println(MenuFacturas.getMenu());
-            System.out.print("Introduce la tarifa a contratar: ");
-            byte opcion = sc.nextByte();
-            if(opcion < 0 || opcion > 2)
-                throw new OpcionIncorrectaException(2);
-            else {
-                MenuCambiarTarifa opcionCambiarTarifa = MenuCambiarTarifa.getOpcion(opcion);
-                opcionCambiarTarifa.ejecutaOpcion(baseDeDatos);
-            }
-            System.out.println("\n\tTarifa especial contratada.\n");
-        } catch (NifNoExistenteException e) {
-            e.printStackTrace();
+        System.out.println("\nCONTRATAR UNA TARIFA ESPECIAL");
+        System.out.println(MenuFacturas.getMenu());
+        System.out.print("Introduce la tarifa a contratar: ");
+        byte opcion = sc.nextByte();
+        if(opcion < 0 || opcion > 2)
+            throw new OpcionIncorrectaException(2);
+        else {
+            MenuCambiarTarifa opcionCambiarTarifa = MenuCambiarTarifa.getOpcion(opcion);
+            opcionCambiarTarifa.ejecutaOpcion(baseDeDatos);
         }
+        System.out.println("\n\tTarifa especial contratada.\n");
     }
 }
