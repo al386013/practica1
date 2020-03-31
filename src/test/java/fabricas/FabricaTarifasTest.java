c9d0b71951f467212778f72377e55d68bb9a9696
al386013 <al386013@uji.es>
2020-03-31 17:06:01 +0200

    nuevo test

--- src/test/java/fabricas/FabricaTarifasTest.java
+++ src/test/java/fabricas/FabricaTarifasTest.java
@@ -1,4 +1,28 @@
 package fabricas;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class FabricaTarifasTest {
+    private static FabricaTarifas fabricaTarifas;
+
+    @BeforeAll
+    public static void inicializa() {
+        fabricaTarifas = new FabricaTarifas();
+    }
+
+    @Test
+    public void getTarifaBasicaTest() {
+
+    }
+
+    @Test
+    public void getTarifaPorHorasTest() {
+
+    }
+
+    @Test
+    public void getTarifaPorDiaTest() {
+
+    }
 }
