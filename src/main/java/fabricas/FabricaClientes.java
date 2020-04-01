35d4816bd18d32cc2ec271108d7750b5b949cc77
al386013 <al386013@uji.es>
2020-04-01 10:27:45 +0200

    af

--- src/main/java/fabricas/FabricaClientes.java
+++ src/main/java/fabricas/FabricaClientes.java
@@ -5,7 +5,6 @@ import datos.clientes.Direccion;
 import datos.clientes.Empresa;
 import datos.clientes.Particular;
 import datos.contrato.tarifas.Tarifa;
-
 import java.io.Serializable;
 
 public class FabricaClientes implements InterfazFabricaClientes, Serializable {
@@ -15,7 +14,7 @@ public class FabricaClientes implements InterfazFabricaClientes, Serializable {
     public Cliente getParticular(String nombre, String apellidos, String telf, String nif, Direccion dir, String email, Tarifa tarifa){
         return new Particular(nombre, apellidos, telf, nif, dir, email, tarifa);
     }
-    
+
     @Override
     public Cliente getEmpresa(String nombre, String telf, String nif, Direccion dir, String email, Tarifa tarifa){
         return new Empresa(nombre, telf, nif, dir, email, tarifa);
